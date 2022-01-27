7e8f6484c28a58d904c4dd6c895c88225e9c78e5
Shawon143 <sishawon07@gmail.com>
2022-01-27 13:53:44 +0600

    search

--- src/Pages/ViewCars/Items.js
+++ src/Pages/ViewCars/Items.js
@@ -1,20 +1,45 @@
 import React, { useState } from "react";
-import { Button, Row, Spinner } from "react-bootstrap";
+import { useEffect } from "react";
+import { Button, FormControl, InputGroup, Row, Spinner } from "react-bootstrap";
 import SingleCar from "../HomePage/SingleCar/SingleCar";
 
 const Items = ({ cars, load }) => {
   const [items, setItems] = useState(cars);
+  const [search, setSearch] = useState("");
 
   const filter = (item) => {
     const updateitem = cars.filter((currentitem) => {
       return currentitem.catagory === item;
     });
-
     setItems(updateitem);
   };
 
+  useEffect(() => {
+    const feat = cars.filter((currentitem) => {
+      return currentitem.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    setItems(feat);
+  }, [search, cars]);
+
+  // const feat = cars.filter((currentitem) => {
+  //   return currentitem.name.toLowerCase().includes(search.toLowerCase());
+  // });
+
   return (
     <div className="text-white">
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Search car"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {/* <Button variant="outline-secondary mx-3" id="button-addon2">
+          Search
+        </Button> */}
+      </InputGroup>
+
       <Button className="mx-3" onClick={() => setItems(cars)}>
         All
       </Button>
